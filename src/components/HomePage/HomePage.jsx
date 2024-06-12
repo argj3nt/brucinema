651abc2e412314c608767aa86854776a27f5c24d
root <root@LAPTOP-TOAEMSPH>
2024-06-12 17:45:54 +0200

    End day push

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -23,30 +23,55 @@ const HomePage = () => {
 
         <div className='movieposter-container'>
           <div className="movieposter-wrapper">
-            <div className="movieposter-overlay"></div>
-            <img src={Poster} alt="poster" className='movieposter' />
+            <div className="movieposter-image-wrapper">
+              <div className="movieposter-overlay">
+                <p className="movie-description"><button className="btn2">SÉANCES</button></p>
+              </div>
+              <img src={Poster} alt="poster" className='movieposter' />
+            </div>
             <p className='moviet'>Peaky Blinders</p>
           </div>
 
           <div className="movieposter-wrapper">
-            <div className="movieposter-overlay"></div>
-            <img src={Poster} alt="poster" className='movieposter' />
+            <div className="movieposter-image-wrapper">
+              <div className="movieposter-overlay">
+                <p className="movie-description"><button className="btn2">SÉANCES</button></p>
+              </div>
+              <img src={Poster} alt="poster" className='movieposter' />
+            </div>
             <p className='moviet'>Peaky Blinders</p>
           </div>
 
           <div className="movieposter-wrapper">
-            <div className="movieposter-overlay"></div>
-            <img src={Poster} alt="poster" className='movieposter' />
+            <div className="movieposter-image-wrapper">
+              <div className="movieposter-overlay">
+                <p className="movie-description"><button className="btn2">SÉANCES</button></p>
+              </div>
+              <img src={Poster} alt="poster" className='movieposter' />
+            </div>
             <p className='moviet'>Peaky Blinders</p>
           </div>
 
           <div className="movieposter-wrapper">
-            <div className="movieposter-overlay"></div>
-            <img src={Poster} alt="poster" className='movieposter' />
+            <div className="movieposter-image-wrapper">
+              <div className="movieposter-overlay">
+                <p className="movie-description"><button className="btn2">SÉANCES</button></p>
+              </div>
+              <img src={Poster} alt="poster" className='movieposter' />
+            </div>
             <p className='moviet'>Peaky Blinders</p>
           </div>
         </div>
       </div>
+      <div className='newsletter'>
+       <h1 className='newstitle'>INSCRIVEZ-VOUS DÈS MAINTENANT !</h1>
+      <div className="input-container">
+        <p className='infonewsletter'>Je souhaite recevoir l'actualité cinéma et les meilleures offres UGC.</p>
+    <input type="email" placeholder="Entrez votre adresse e-mail" className="email-input" />
+
+    <button className="subscribe-btn">JE M'ABONNE</button>
+  </div>
+</div>
     </>
   );
 }
